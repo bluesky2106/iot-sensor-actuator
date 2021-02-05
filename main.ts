419d334c708a17b4c07d8999e4f529c3279a260b
bluesky2106 <60764284+bluesky2106@users.noreply.github.com>
2021-02-05 19:39:11 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    cmd = receivedNumber
+    if (cmd == 0) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+    if (cmd == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+    if (cmd == 2) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    }
+    if (cmd == 3) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    }
+})
+let cmd = 0
+radio.setGroup(77)
+pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
+let count = 0
+basic.forever(function () {
+    count += 1
+    if (count == 100) {
+        radio.sendNumber(input.temperature())
+        count = 0
+    }
+    basic.pause(100)
+})

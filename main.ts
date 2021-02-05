726bdd93a149e116854cfd09cc3284fc490c8029
bluesky2106 <60764284+bluesky2106@users.noreply.github.com>
2021-02-05 20:06:14 +0700

    update command type string

--- main.ts
+++ main.ts
@@ -1,23 +1,23 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    cmd = receivedNumber
-    if (cmd == 0) {
+radio.onReceivedString(function (receivedString) {
+    cmd = receivedString
+    if (cmd == "0") {
         pins.digitalWritePin(DigitalPin.P0, 0)
         pins.digitalWritePin(DigitalPin.P1, 0)
     }
-    if (cmd == 1) {
+    if (cmd == "1") {
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 0)
     }
-    if (cmd == 2) {
+    if (cmd == "2") {
         pins.digitalWritePin(DigitalPin.P0, 0)
         pins.digitalWritePin(DigitalPin.P1, 1)
     }
-    if (cmd == 3) {
+    if (cmd == "3") {
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 1)
     }
 })
-let cmd = 0
+let cmd = ""
 radio.setGroup(77)
 pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
